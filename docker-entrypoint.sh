d0c64e1ff1394217f1ed95016233dd0990e5177b
Yacha-Shan <xxyachagalaxyxx@gmail.com>
2026-05-19 19:33:56 +0200

    fix: utilisation schema:update au lieu des migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "=== Lancement des migrations ==="
-php /var/www/html/bin/console doctrine:migrations:migrate --no-interaction --env=prod
+echo "=== Création du schéma de base de données ==="
+php /var/www/html/bin/console doctrine:schema:update --force --env=prod
 
 echo "=== Démarrage Apache ==="
 exec "$@"
